2e794f4b32434176bf4e8577e22dfe18232d1323
Zainal Abidin <okakzai@gmail.com>
2016-12-08 12:45:42 +0700

    Update admob.js

--- admob.js
+++ admob.js
@@ -65,9 +65,6 @@ function initAds() {
   }
 }
 
-function onMapInit(map) {
-}
-
 function onDeviceReady() {
   document.removeEventListener('deviceready', onDeviceReady, false);
   initAds();
@@ -77,11 +74,20 @@ function onDeviceReady() {
   // request an interstitial
   admob.requestInterstitialAd();
   
-  var mapDiv = document.getElementById("map_canvas");
-  // Initialize the map plugin
-  var map = plugin.google.maps.Map.getMap(mapDiv);
-  // You have to wait the MAP_READY event.
-  map.on(plugin.google.maps.event.MAP_READY, onMapInit);
 }
 
 document.addEventListener("deviceready", onDeviceReady, false);
+
+document.addEventListener("deviceready", function() {
+  // Define a div tag with id="map_canvas"
+  var mapDiv = document.getElementById("map_canvas");
+
+  // Initialize the map plugin
+  var map = plugin.google.maps.Map.getMap(mapDiv);
+
+  // You have to wait the MAP_READY event.
+  map.on(plugin.google.maps.event.MAP_READY, onMapInit);
+});
+
+function onMapInit(map) {
+}
